25ad958830e38b9fb137369cd958e3d0d7fd5033
Marcus Costa <marcusvinicius2097@gmail.com>
2025-04-14 13:57:08 -0300

    First Commit

---
+++ src/main/java/desafioPOO/Main.java
@@ -0,0 +1,27 @@
+package desafioPOO;
+
+public class Main {
+    public static void main(String[] args){
+        Curso curso = new Curso();
+        curso.setTitulo("Java");
+        curso.setDescricao("Aula em JAVA");
+        curso.setCargaHoraria(65);
+
+        System.out.println(curso);
+
+        Curso cursoJS = new Curso();
+        cursoJS.setTitulo("JavaScript");
+        cursoJS.setDescricao("Aula em JavaScript");
+        cursoJS.setCargaHoraria(40);
+
+        System.out.println(cursoJS);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria JavaScript");
+        mentoria.setDescricao("mentoria em JavaScript");
+        mentoria.setCargaHoraria(15);
+        System.out.println(mentoria);
+
+
+    }
+}
