8d979a2e6a05c3491ba179da949a46bac9443e0c
Marcus Costa <marcusvinicius2097@gmail.com>
2025-04-14 16:06:24 -0300

    implementando as classes

--- src/main/java/desafioPOO/Main.java
+++ src/main/java/desafioPOO/Main.java
@@ -24,6 +24,30 @@ public class Main {
         mentoria.setData(LocalDate.now());
         System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp JAVA Devloper");
+        bootcamp.setDescricao("Descrição Bootcamp JAVA Devloper");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(cursoJS);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMarcus = new Dev();
+        devMarcus.setNome("Marcus");
+        devMarcus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Marcus " + devMarcus.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos Marcus " + devMarcus.getConteudosConcluidos());
+
+        devMarcus.progrendir();
+        System.out.println("Conteudos Inscritos Marcus " + devMarcus.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos Marcus " + devMarcus.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcus.calcularTotalXP());
+
+        Dev devAna =  new Dev();
+        devAna.setNome("Ana");
+        devAna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Ana " + devAna.getConteudosInscritos());
+        System.out.println("Conteudos Inscritos Ana " + devAna.getConteudosConcluidos());
+
 
     }
 }
